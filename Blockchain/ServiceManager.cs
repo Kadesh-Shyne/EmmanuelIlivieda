46585dbb8e30af2401de6527c522c69e05dae0e3
Ilivieda Emmanuel <ilivieda1@yahoo.com>
2023-04-10 12:22:23 +0100

    Few Changes To Hotel Management System

---
+++ Blockchain/ServiceManager.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Hotel_Management_Application.Contracts;
+using Hotel_Management_Service.Contract;
+using Hotel_Management_System.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Service.Contracts;
+
+namespace Hotel_Management_System
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<IAuthenticationService> _authenticationService_;
+
+        public ServiceManager(IRepositoryManager repositoryManager,
+            IMapper mapper,
+            UserManager<CustomerDetails> userManager,
+            IConfiguration configuration)
+        {
+            _authenticationService = new Lazy<IAuthenticationService>(() =>
+                new AuthenticationService(mapper, userManager, configuration));
+        }
+        public IAuthenticationService AuthenticationService => _authenticationService_.Value;
+    }
+}
